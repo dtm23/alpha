e2df54baf1fa615e300460a8a5fa270b39c975ad
Andrew Cunliffe <andrew.c.cunliffe@gmail.com>
2014-04-01 19:55:14 +0100

    Added simple registration UI

--- src/front/public/controllers/RegistrationCtrl.js
+++ src/front/public/controllers/RegistrationCtrl.js
@@ -1,3 +1,15 @@
-app.controller('RegistrationCtrl', ['$scope', function($scope) {
+app.controller('RegistrationCtrl', ['$scope', '$resource', function($scope, $resource) {
+    var registration = $resource('/api/register');
 
+    $scope.clear = function() {
+        $scope.error = false;
+    };
+
+    $scope.submit = function(account) {
+        registration.save(account, function() {
+            $scope.success = true;
+        }, function(error) {
+            $scope.error = error.data;
+        });
+    };
 }]);
--- test/front/public/RegistrationCtrl.spec.js
+++ test/front/public/RegistrationCtrl.spec.js
@@ -1,16 +1,46 @@
 describe('Public > Registration Controller', function() {
-    var ctrl, scope;
+    var ctrl, scope, http;
 
     beforeEach(module('alpha'));
 
     beforeEach(function() {
         inject(function ($injector, $rootScope, $controller) {
             scope = $rootScope.$new();
+            http = $injector.get('$httpBackend');
 
             ctrl = $controller('RegistrationCtrl', { $scope: scope });
         });
     });
 
-    it('should exist', function() {
+    var account = {
+        username: "andrew@test.com",
+        password: "password"
+    };
+
+    it('should allow you to clear errors', function() {
+        scope.error = true;
+
+        scope.clear();
+
+        expect(scope.error).toBeFalsy();
+    });
+
+    it('should allow you to submit a valid registration', function() {
+        http.expectPOST('/api/register', account).respond(200);
+
+        scope.submit(account);
+        http.flush();
+
+        expect(scope.success).toBeTruthy();
+    });
+
+    it('should set a flag when registration fails', function() {
+        http.expectPOST('/api/register', account).respond(400, { code: 1, message: "Failed" });
+
+        scope.submit(account);
+        http.flush();
+
+        expect(scope.error.code).toBe(1);
+        expect(scope.error.message).toBe("Failed");
     });
 });
