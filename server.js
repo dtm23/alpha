cb40c74b3edebbfeabb8d5e2c1fa7e9689c0d2da
Andrew Cunliffe <andrew.c.cunliffe@gmail.com>
2014-04-24 22:06:42 +0100

    Updated to support ExpressJS 4.0

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 // Dependencies
 var express = require('express'),
-    mysql = require('mysql'),
     http = require('http'),
-    path = require('path');
+    path = require('path'),
+    mysql = require('mysql'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    compress = require('compression'),
+    override = require('method-override'),
+    cookie = require('cookie-parser'),
+    session = require('express-session'),
+    errorHandler = require('errorhandler');
 
 // Set up the application
 var app = express();
@@ -14,20 +21,14 @@ app.set('port', process.env.PORT || 1337);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Favicon
-app.use(express.favicon());
-
 // Express Configuration
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.compress());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.bodyParser());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'assets')));
+app.use(bodyParser());
+app.use(compress());
+app.use(override());
+app.use(cookie());
+app.use(session({ secret: 'cookie-secret', key: 'sid', cookie: { secure: true } }));
+app.use(logger());
 
 // Database Connection
 app.set('DB:connection', mysql.createPool({
@@ -44,7 +45,7 @@ require('./config/database.tables.js')(app);
 
 // Development (set NODE_ENV environment variable to trigger this)
 if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
+    app.use(errorHandler());
     require('./config/database.data.js')(app);
 }
 
